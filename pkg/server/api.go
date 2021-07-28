0b7c0c94787f124f7af7e963e0e17c791237cb36
saplaza1410 <58708131+saplaza1410@users.noreply.github.com>
2021-07-27 19:05:24 -0500

    Create api.go

---
+++ pkg/server/api.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type api struct {
+	router http.Handler
+}
+
+type Server interface {
+	Router() http.Handler
+}
+
+func New() Server {
+	a := &api{}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/gophers", a.fetchGophers).Methods(http.MethodGet)
+	r.HandleFunc("/gophers/{ID:[a-zA-Z0-9_]+}", a.fetchGopher).Methods(http.MethodGet)
+
+	a.router = r
+	return a
+}
+
+func (a *api) Router() http.Handler {
+	return a.router
+}
